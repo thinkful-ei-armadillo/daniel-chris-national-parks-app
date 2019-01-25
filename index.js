db4d664881afa2c37d995db19f3bc76485b19c07
Daniel Ors <do7811@gmail.com>
2019-01-25 17:16:33 -0500

    final commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function formatQueryParams(params) {
 function displayResults(responseJson) {
   console.log(responseJson);
   $('.results-list').empty();
-  for (let i = 0; i < responseJson.data.length; i++){
+  for ( let i = 0; i < responseJson.data.length; i++ ){
     $('.results-list').append(
       `<div class="results-wrapper">
       <h3 for="park-name">${responseJson.data[i].name}</h3>
