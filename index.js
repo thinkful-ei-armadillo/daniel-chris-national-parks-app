56a07d1753b90d3904e3e4185d503da550f750c2
Daniel Ors <do7811@gmail.com>
2019-01-25 17:02:30 -0500

    final commit

--- index.js
+++ index.js
@@ -4,30 +4,17 @@
 const api_key = 'IIwKvgVyokkngr9dQP4DXTYHTiIWMx2yJ5PHF2L';
 const BASE_URL = 'https://api.nps.gov/api/v1/parks/';
 
-// function getAllParks( searchTerm, query = {}, maxResults ) {
-  
-// }
 
 function formatQueryParams(params) {
-  for ( let i = 0; i < params.stateCode.length; i++ ) {
-    const multKeys = params.stateCode.split( ' ' );
-
-  }
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
 function displayResults(responseJson) {
-  // if there are previous results, remove them
   console.log(responseJson);
   $('.results-list').empty();
-  // iterate through the items array
   for (let i = 0; i < responseJson.data.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
     $('.results-list').append(
       `<div class="results-wrapper">
       <h3 for="park-name">${responseJson.data[i].name}</h3>
@@ -36,8 +23,6 @@ function displayResults(responseJson) {
       <a href='${responseJson.data[i].url}' for="link to park page">Link</a>
       </div>`
     )};
-  //display the results section  
-  // $('#results').removeClass('hidden');
 }
 
 function getParks(query, limit=10) {
